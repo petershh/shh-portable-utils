5655d1678d9ced83c0f69159f6dcaef8b6d9f396
petershh <petershh@disroot.org>
2022-07-15 11:58:25 +0300

    Basename fixes

--- src/shh-portable-utils/basename.c
+++ src/shh-portable-utils/basename.c
@@ -1,15 +1,18 @@
 #include <string.h>
 
+#include <skalibs/strerr2.h>
 #include <skalibs/allreadwrite.h>
+
+#define USAGE "basename string [suffix]"
 
 int main(int argc, char **argv)
 {
     size_t sufflen, len;
     char *basename_start;
-    if (argc < 2) {
-        allwrite(1, ".\n", 2);
-        return 0;
-    }
+    PROG = "basename";
+    if (argc < 2)
+        strerr_dieusage(100, USAGE);
+
     len = strlen(argv[1]);
     if (!len) {
         allwrite(1, ".\n", 2);
