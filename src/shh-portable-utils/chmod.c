209e0fd80abba905559c8542bf4adfcde2c4445d
petershh <petershh@disroot.org>
2022-07-31 14:38:46 +0300

    Begin separating chmod-related functions to their own files

--- src/shh-portable-utils/chmod.c
+++ src/shh-portable-utils/chmod.c
@@ -11,32 +11,9 @@
 #include <skalibs/stralloc.h>
 #include <skalibs/genalloc.h>
 
+#include "shhfuncs.h"
+
 #define USAGE "chmod [-R] mode file..."
-
-#define S_ISALL (S_IRWXU | S_IRWXG | S_IRWXO | S_ISUID | S_ISGID | S_ISVTX)
-
-struct chmod_directive_s {
-    mode_t who;
-    int action;
-    mode_t perm;
-    int permcopy;
-    int dir_x;
-};
-
-typedef struct chmod_directive_s chmod_directive;
-
-#define CHMOD_DIRECTIVE_ZERO { 0, 0, 0, 0, 0 }
-
-mode_t parse_octal(char const*);
-
-void parse_symbolic(char const*, genalloc*);
-
-/*
- * Algorithm for chmod mode parsing was inspired by sbase's chmod
- * implementation (https://core.suckless.org/sbase), so let's credit them here.
- */
-
-int change_mode(char const*, mode_t, genalloc*, mode_t);
 
 int traverse_dir(stralloc*, mode_t, genalloc*, mode_t);
 
