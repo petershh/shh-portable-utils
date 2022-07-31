ba506d93e0954f02f13dbba742c83387b3bbd077
petershh <petershh@disroot.org>
2022-07-31 15:35:39 +0300

    Move parse_octal, parse_symbolic and change_mode to their own files

---
+++ src/shh-portable-utils/parse_symbolic.c
@@ -0,0 +1,127 @@
+#include <skalibs/genalloc.h>
+#include <skalibs/strerr2.h>
+
+#include "shhfuncs.h"
+
+void parse_symbolic(char const *raw, genalloc *directives)
+{
+    char const *p = raw;
+    for (;;) {
+        chmod_directive d = CHMOD_DIRECTIVE_ZERO;
+        /* stage 1: parse 'who' list */
+        for (;;) {
+            if (*p == 'u')
+                d.who |= S_IRWXU | S_ISUID;
+            else if (*p == 'g')
+                d.who |= S_IRWXG | S_ISGID;
+            else if (*p == 'o')
+                d.who |= S_IRWXO;
+            else if (*p == 'a')
+                d.who |= S_IRWXU | S_IRWXG | S_IRWXO | S_ISUID | S_ISGID;
+            else
+                break;
+            p++;
+        }
+        /* stage 2: actions. First cycle is out of the loop. */
+        switch(*p) {
+            case '+':
+            case '-':
+            case '=':
+                d.action = *p;
+                break;
+            default:
+                strerr_dief2x(100, "invalid mode: ", raw);
+        }
+        p++;
+        for (;;) {
+            if (*p == 'u')
+                d.permcopy |= 1;
+            else if (*p == 'g')
+                d.permcopy |= 2;
+            else if (*p == 'o')
+                d.permcopy |= 4;
+            else if (*p == 'r')
+                d.perm |= S_IRUSR | S_IRGRP | S_IROTH;
+            else if (*p == 'w')
+                d.perm |= S_IWUSR | S_IWGRP | S_IWOTH;
+            else if (*p == 'x')
+                d.perm |= S_IXUSR | S_IXGRP | S_IXOTH;
+            else if (*p == 'X')
+                d.dir_x = 1;
+            else if (*p == 's')
+                d.perm |= S_ISUID | S_ISGID;
+            else if (*p == 't')
+                d.perm |= S_ISVTX;
+            else if (*p == ',') {
+                p++;
+                break;
+            }
+            else if (*p == '\0') {
+                if (d.perm && d.permcopy)
+                    strerr_dief2x(100, "invalid mode: ", raw);
+                if (!genalloc_append(chmod_directive, directives, &d))
+                    strerr_diefu1sys(111, "parse mode");
+                return;
+            }
+            p++;
+        }
+
+        if (d.perm && d.permcopy)
+            strerr_dief2x(100, "invalid mode: ", raw);
+        if (!genalloc_append(chmod_directive, directives, &d))
+            strerr_diefu1sys(111, "parse mode");
+
+        for (;;) {
+            d.action = d.perm = d.permcopy = d.dir_x = 0;
+            if (*p == '+' || *p == '-' || *p == '=')
+                d.action = *p;
+            else if (*p == ',')
+                break;
+            else if (*p == '\0')
+                return;
+            else
+                strerr_dief2x(100, "invalid mode: ", raw);
+
+            p++;
+
+            for (;;) {
+                if (*p == 'u')
+                    d.permcopy |= 1;
+                else if (*p == 'g')
+                    d.permcopy |= 2;
+                else if (*p == 'o')
+                    d.permcopy |= 4;
+                else if (*p == 'r')
+                    d.perm |= S_IRUSR | S_IRGRP | S_IROTH;
+                else if (*p == 'w')
+                    d.perm |= S_IWUSR | S_IWGRP | S_IWOTH;
+                else if (*p == 'x')
+                    d.perm |= S_IXUSR | S_IXGRP | S_IXOTH;
+                else if (*p == 'X')
+                    d.dir_x = 1;
+                else if (*p == 's')
+                    d.perm |= S_ISUID | S_ISGID;
+                else if (*p == 't')
+                    d.perm |= S_ISVTX;
+                else if (*p == ',') {
+                    p++;
+                    break;
+                }
+                else if (*p == '\0') {
+                    if (d.perm && d.permcopy)
+                        strerr_dief2x(100, "invalid mode: ", raw);
+                    if (!genalloc_append(chmod_directive, directives, &d))
+                        strerr_diefu1sys(111, "parse mode");
+                    return;
+                }
+                p++;
+            }
+
+            if (d.perm && d.permcopy)
+                strerr_dief2x(100, "invalid mode: ", raw);
+            if (!genalloc_append(chmod_directive, directives, &d))
+                strerr_diefu1sys(111, "parse mode");
+        }
+    }
+}
+
