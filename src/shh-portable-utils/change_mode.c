b516a7cd16ae156a211372acd9f0ba5a25792e49
petershh <petershh@disroot.org>
2022-08-03 00:02:08 +0300

    chmod: do not pass genalloc to functions when not needed

--- src/shh-portable-utils/change_mode.c
+++ src/shh-portable-utils/change_mode.c
@@ -1,6 +1,5 @@
 #include <sys/stat.h>
 
-#include <skalibs/genalloc.h>
 #include <skalibs/strerr2.h>
 
 #include "shhfuncs.h"
@@ -10,26 +9,25 @@
  * implementation (https://core.suckless.org/sbase), so let's credit them here.
  */
 
-mode_t change_mode(mode_t initial_mode, genalloc *directives, mode_t mask)
+mode_t change_mode(mode_t initial_mode, chmod_directive *directives,
+                   size_t directives_len, mode_t mask)
 {
     mode_t cur_mode, who, perm, clear;
-    chmod_directive const *s = genalloc_s(chmod_directive, directives);
-    size_t len = genalloc_len(chmod_directive, directives);
 
     cur_mode = initial_mode;
 
-    for (size_t i = 0; i < len; i++) {
-        if (s[i].who) {
-            who = s[i].who;
-            clear = s[i].who;
+    for (size_t i = 0; i < directives_len; i++) {
+        if (directives[i].who) {
+            who = directives[i].who;
+            clear = directives[i].who;
         }
         else {
             who = ~mask;
             clear = S_ISALL;
         }
-        perm = s[i].perm;
+        perm = directives[i].perm;
 
-        if (s[i].permcopy & 1) {
+        if (directives[i].permcopy & 1) {
             if (cur_mode & S_IRUSR)
                 perm |= S_IRUSR | S_IRGRP | S_IROTH;
             if (cur_mode & S_IWUSR)
@@ -37,7 +35,7 @@ mode_t change_mode(mode_t initial_mode, genalloc *directives, mode_t mask)
             if (cur_mode & S_IXUSR)
                 perm |= S_IXUSR | S_IXGRP | S_IXOTH;
         }
-        if (s[i].permcopy & 2) {
+        if (directives[i].permcopy & 2) {
             if (cur_mode & S_IRGRP)
                 perm |= S_IRUSR | S_IRGRP | S_IROTH;
             if (cur_mode & S_IWGRP)
@@ -45,7 +43,7 @@ mode_t change_mode(mode_t initial_mode, genalloc *directives, mode_t mask)
             if (cur_mode & S_IXGRP)
                 perm |= S_IXUSR | S_IXGRP | S_IXOTH;
         }
-        if (s[i].permcopy & 4) {
+        if (directives[i].permcopy & 4) {
             if (cur_mode & S_IROTH)
                 perm |= S_IRUSR | S_IRGRP | S_IROTH;
             if (cur_mode & S_IWOTH)
@@ -54,11 +52,12 @@ mode_t change_mode(mode_t initial_mode, genalloc *directives, mode_t mask)
                 perm |= S_IXUSR | S_IXGRP | S_IXOTH;
         }
 
-        if (s[i].dir_x && (S_ISDIR(cur_mode)
-                            || (cur_mode & (S_IXUSR | S_IXGRP | S_IXOTH))))
+        if (directives[i].dir_x && (S_ISDIR(cur_mode)
+                                    || (cur_mode
+                                        & (S_IXUSR | S_IXGRP | S_IXOTH))))
             perm |= S_IXUSR | S_IXGRP | S_IXOTH;
 
-        switch (s[i].action) {
+        switch (directives[i].action) {
             case '=':
                 cur_mode &= ~clear;
             case '+':
